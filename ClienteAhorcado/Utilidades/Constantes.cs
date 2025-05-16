350d51db24d20fc269b34a19eeb7565120547f31
kaleb746 <iankaleb746@gmail.com>
2025-05-15 23:40:57 -0600

    Se añadio los estilos para Botones, TextBlock y ComboBox, Animacion para validar entradas

---
+++ ClienteAhorcado/Utilidades/Constantes.cs
@@ -0,0 +1,8 @@
+﻿namespace ClienteAhorcado.Utilidades
+
+{
+    public static class Constantes
+    {
+        public static readonly string PATRON_TEXTO_GENERAL = @"[^A-Za-zÁÉÍÓÚÜÑáéíóúüñ0-9\s\-\.,()/#]+$";
+    }
+}
